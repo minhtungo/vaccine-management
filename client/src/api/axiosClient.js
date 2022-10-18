7e1653a58c2659e93ebc3123cab39d7407e48e86
minhtungo <loducfc@gmail.com>
2022-10-18 18:41:53 -0400

    changed base url

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,32 +1,35 @@
-import axios from "axios"
-import queryString from 'query-string'
+import axios from 'axios';
+import queryString from 'query-string';
 
-const baseUrl = 'https://vaccine-management.herokuapp.com/';
-const getToken = () => localStorage.getItem('token')
+const baseUrl = 'https://vaccine-management.herokuapp.com/api/';
+const getToken = () => localStorage.getItem('token');
 
 const axiosClient = axios.create({
-    baseURL: baseUrl,
-    paramsSerializer: params => queryString.stringify({params})
-})
+  baseURL: baseUrl,
+  paramsSerializer: (params) => queryString.stringify({ params }),
+});
 
 axiosClient.interceptors.request.use(async (config) => {
-    return {
-        ...config,
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${getToken()}`
-        }
-    }
-})
+  return {
+    ...config,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${getToken()}`,
+    },
+  };
+});
 
-axiosClient.interceptors.response.use((response) => {
-    if (response && response.data) return response.data
-    return response
-}, (err) => {
+axiosClient.interceptors.response.use(
+  (response) => {
+    if (response && response.data) return response.data;
+    return response;
+  },
+  (err) => {
     if (!err.response) {
-        alert('Err! Netword err!')
+      alert('Err! Netword err!');
     }
-    throw err
-})
+    throw err;
+  }
+);
 
-export default axiosClient
+export default axiosClient;
